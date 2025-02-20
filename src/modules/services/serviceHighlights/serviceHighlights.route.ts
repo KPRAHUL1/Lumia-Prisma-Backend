6682d9d49ea1bc1f547187ad1967074440e7b977
Rahul K <rahulsachin07314@gmail.com>
2025-02-20 12:20:16 +0530

    hi

--- src/modules/services/serviceHighlights/serviceHighlights.route.ts
+++ src/modules/services/serviceHighlights/serviceHighlights.route.ts
@@ -1,36 +1,36 @@
-import { addServiceHighlights, getAllServiceHighlights, updateServiceHighlights, deleteServiceHighlightsById, getServiceHighlightsById } from "./serviceHighlights.service";
+// import { addServiceHighlights, getAllServiceHighlights, updateServiceHighlights, deleteServiceHighlightsById, getServiceHighlightsById } from "./serviceHighlights.service";
 
 
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
 
-router.post('/', async (req:any, res: any) => {
-    const payload= req.body;
-    const result = await addServiceHighlights(payload);
-    res.status(201).json(result)
-})
+// router.post('/', async (req:any, res: any) => {
+//     const payload= req.body;
+//     const result = await addServiceHighlights(payload);
+//     res.status(201).json(result)
+// })
 
-router.get('/',async (req:any,res:any)=>{
-    const serviceId = req.params.serviceId;
-    const response= await getAllServiceHighlights(serviceId);
-    res.status(200).json(response)
-})
-router.get("/:id", async (req: any, res: any) => {
-    const id = req.params.id;
-    const response = await getServiceHighlightsById(id);
-    res.status(200).json(response);
-  });
-router.put('/:id',async(req:any, res: any) => {
-    const id =req.params.id
-    const payload= req.body;
-    const result = await updateServiceHighlights(id,payload);
-    res.status(200).json(result)
-})
+// router.get('/',async (req:any,res:any)=>{
+//     const serviceId = req.params.serviceId;
+//     const response= await getAllServiceHighlights(serviceId);
+//     res.status(200).json(response)
+// })
+// router.get("/:id", async (req: any, res: any) => {
+//     const id = req.params.id;
+//     const response = await getServiceHighlightsById(id);
+//     res.status(200).json(response);
+//   });
+// router.put('/:id',async(req:any, res: any) => {
+//     const id =req.params.id
+//     const payload= req.body;
+//     const result = await updateServiceHighlights(id,payload);
+//     res.status(200).json(result)
+// })
 
-router.delete('/:id', async(req:any, res:any)=>{
-    const id =req.params.id
-    await deleteServiceHighlightsById(id)
-    res.status(200).json("Deleted successfully")
-})
-module.exports = router;
+// router.delete('/:id', async(req:any, res:any)=>{
+//     const id =req.params.id
+//     await deleteServiceHighlightsById(id)
+//     res.status(200).json("Deleted successfully")
+// })
+// module.exports = router;
--- src/modules/services/serviceHighlights/serviceHighlights.service.ts
+++ src/modules/services/serviceHighlights/serviceHighlights.service.ts
@@ -1,57 +1,57 @@
-import { db } from "../../../shared/lib/db"
+// import { db } from "../../../shared/lib/db"
 
 
- export async function addServiceHighlights(payload : any){
-    return await db.serviceHighlights.create({
-        data: {
-            name: payload.name,
-            icon: payload.icon,
-            description: payload.description,
-            imagePath: payload.imagePath,
-            order:payload.order,
-            serviceId:payload.serviceId,
-            isActive:payload.isActive,
-        }
-    })
-}
-export async function getAllServiceHighlights(serviceId:string){
-   return await db.serviceHighlights.findMany({
-    where:{
-        serviceId:serviceId,
-        deletedAt:null
-    }
-   })  
-}
-export async function getServiceHighlightsById(id: string) {
+//  export async function addServiceHighlights(payload : any){
+//     return await db.serviceHighlights.create({
+//         data: {
+//             name: payload.name,
+//             icon: payload.icon,
+//             description: payload.description,
+//             imagePath: payload.imagePath,
+//             order:payload.order,
+//             serviceId:payload.serviceId,
+//             isActive:payload.isActive,
+//         }
+//     })
+// }
+// export async function getAllServiceHighlights(serviceId:string){
+//    return await db.serviceHighlights.findMany({
+//     where:{
+//         serviceId:serviceId,
+//         deletedAt:null
+//     }
+//    })  
+// }
+// export async function getServiceHighlightsById(id: string) {
   
-    return db.serviceHighlights.findFirst({
-      where: {
-        id: id,
-        deletedAt:null
-        },
-    });
-  }
-export async function updateServiceHighlights(id : string, payload : any){
-    return db.serviceHighlights.update({
-        where: {
-            id: id,
-          },
-          data: {
-            name: payload.name,
-            icon: payload.icon,
-            description: payload.description,
-            imagePath: payload.imagePath,
-            order:payload.order,
-            isActive:payload.isActive,
-            serviceId:payload.serviceId,
-            updatedAt: new Date()
-        }
-    })
-} 
-export async function deleteServiceHighlightsById(id: string){
-    return db.serviceHighlights.delete({
-        where: {
-            id: id,
-          },
-    })
-}
+//     return db.serviceHighlights.findFirst({
+//       where: {
+//         id: id,
+//         deletedAt:null
+//         },
+//     });
+//   }
+// export async function updateServiceHighlights(id : string, payload : any){
+//     return db.serviceHighlights.update({
+//         where: {
+//             id: id,
+//           },
+//           data: {
+//             name: payload.name,
+//             icon: payload.icon,
+//             description: payload.description,
+//             imagePath: payload.imagePath,
+//             order:payload.order,
+//             isActive:payload.isActive,
+//             serviceId:payload.serviceId,
+//             updatedAt: new Date()
+//         }
+//     })
+// } 
+// export async function deleteServiceHighlightsById(id: string){
+//     return db.serviceHighlights.delete({
+//         where: {
+//             id: id,
+//           },
+//     })
+// }
